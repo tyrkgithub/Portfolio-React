2b0dba8b87360e566b7639e84c053f345db1620e
tyrkgitgub <tiayorke@hotmail.co.uk>
2023-03-13 22:37:16 +0000

    code added for working download button

--- portfolio/src/Components/CV/cv.js
+++ portfolio/src/Components/CV/cv.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Project from "../Projects/project";
-import Resume from "../../Assets/colorPal.png";
+import Resume from "../../Assets/TiaCV.png";
+import Download from "./download";
 
 function CV() {
   return (
-    <div className="container-fluid d-flex justify-content-center">
-      <Project imgsrc={Resume} title="View Resume" href="" />
+    <div>
+      <div className="container-fluid d-flex justify-content-center">
+        <Project imgsrc={Resume} title="My Resume" href="" />
+      </div>
+      <Download />
     </div>
   );
 }
